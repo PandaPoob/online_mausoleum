3c5fac692efc8d08797343552acc886e6336beea
Freja Smith <frejasmith123@gmail.com>
2024-12-08 20:27:13 +0100

    added to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    screens: {
+      sm: "30rem", //480px
+      md: "48rem", //768px
+      lg: "64rem", //1024px
+      xl: "80rem", //1280px
+      xxl: "100rem", //1600px
+    },
+    colors: {
+      bg_black: "#0F0F0F",
+      primary: "#1F1F21",
+      secondary: "#8B9093",
+      white: "#E9E9E9",
     },
   },
   plugins: [],
