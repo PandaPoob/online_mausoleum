a2dfc06292ea68ad7ecf247bf44a81a828830dc9
Freja Smith <frejasmith123@gmail.com>
2024-12-22 17:52:28 +0100

    Added title to document

--- src/app/(pages)/questions/page.tsx
+++ src/app/(pages)/questions/page.tsx
@@ -1,4 +1,9 @@
 import Questions from "@/app/_views/Questions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Spørgsmål & Svar",
+};
 
 function QuestionsPage() {
   return <Questions />;
